7938436568a94bc813652a7e255fadd0b9dc284d
jorge1727 <jorge1727.jt@gmail.com>
2024-02-16 10:58:24 +0100

    Añadido Categoria con su correspondiente controller y service para el crud

---
+++ src/main/java/org/iesvdm/videoclub/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package org.iesvdm.videoclub.repository;
+
+import org.iesvdm.videoclub.domain.Categoria;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface CategoriaRepository extends JpaRepositoryImplementation<Categoria, Long> {
+}
--- src/test/java/org/iesvdm/videoclub/VideoclubApplicationTests.java
+++ src/test/java/org/iesvdm/videoclub/VideoclubApplicationTests.java
@@ -1,13 +1,25 @@
 package org.iesvdm.videoclub;
 
+import org.iesvdm.videoclub.repository.PeliculaRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VideoclubApplicationTests {
 
+    @Autowired
+    PeliculaRepository peliculaRepository;
+
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testPeliCategoria(){
+
+
     }
 
 }
